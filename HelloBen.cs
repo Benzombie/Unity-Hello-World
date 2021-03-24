e9aa376c8d3d6db904907be3a9369f67a63c301a
Benjamin Waters <55147220+Benzombie@users.noreply.github.com>
2021-03-23 20:44:03 -0400

    Add files via upload

---
+++ HelloBen.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloBen : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Hello Ben");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
